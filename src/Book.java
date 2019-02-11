cc4bc5842b692caae66a6ccd2deaefd724c12be6
Haklerz <haakon.lervik@gmail.com>
2019-02-11 11:25:14 +0100

    yeet

--- src/Book.java
+++ src/Book.java
@@ -11,19 +11,22 @@ package src;
  * </ul>
  */
 public class Book {
-    private String title, author;
-    private int pages;
+    private String title, author, publisher;
+    private int pages, edition;
 
     /**
      * Constructor for the class Book.
      * @param title Title of the Book.
      * @param author Author of the Book.
      * @param pages Number of pages in the Book.
+     * @param publisher Publisher of the Book.
      */
-    public Book(String title, String author, int pages) {
+    public Book(String title, String author, String publisher, int pages, int edition) {
         this.title = title;
         this.author = author;
+        this.publisher = publisher;
         this.pages = pages;
+        this.edition = edition;
     }
 
     /**
@@ -43,10 +46,26 @@ public class Book {
     }
 
     /**
+     * Returns the publisher of the Book as a<code>String</code>.
+     * @return Publisher of the Book.
+     */
+    public String getPublisher() {
+        return this.publisher;
+    }
+
+    /**
      * Returns the number of pages in the Book as an<code>int</code>.
      * @return Number of pages in the Book.
      */
     public int getPages() {
         return this.pages;
+    }
+
+    /**
+     * Returns the edition of the book.
+     * @return Edition of the Book.
+     */
+    public int getEdition() {
+        return this.edition;
     }
 }
--- src/Registry.java
+++ src/Registry.java
@@ -8,17 +8,44 @@ import java.util.ArrayList;
  * With this class you can:
  * <ul>
  * <li>Add a book to the registry.
+ * <li>List the contents of the Registry.
+ * <li>Search the Registry by title.
+ * <li>Search the Registry by author.
+ * <li>Search the Registry by publisher.
+ * <li>Add an existing book to a series.
  * </ul>
  * @see Book
  */
 public class Registry {
     private ArrayList<Book> books;
 
+    /**
+     * 
+     */
     public Registry() {
         this.books = new ArrayList<>();
     }
 
+    /**
+     * 
+     */
     public void addBook(Book book) {
         books.add(book);
+    }
+
+    /**
+     * Finds books by title.
+     * @return ArrayList of found books.
+     */
+    public ArrayList<Book> findByTitle(String title) {
+        ArrayList<Book> foundBooks = new ArrayList<>();
+        for (Book book : books) {
+            String bookTitle = book.getTitle().toUpperCase();
+            String testTitle = title.toUpperCase();
+            if (bookTitle.contains(testTitle)) {
+                foundBooks.add(book);
+            }
+        }
+        return foundBooks;
     }
 }
