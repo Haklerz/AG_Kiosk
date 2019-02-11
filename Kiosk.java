b0dedffbd85c844930bc6eca7f7e956a3a76b351
ComradePetah <petah92@gmail.com>
2019-02-11 11:54:42 +0100

    Testing git

---
+++ Kiosk.java
@@ -0,0 +1,17 @@
+/**
+ * Represents the kiosk
+ * 
+ * @author Peter Osnes
+ * @version v0.1 2019-02-06
+ */
+
+public class Kiosk
+{
+    /**
+     * Constructor for the objects of class Kiosk
+     */
+    public Kiosk()
+    {
+        
+    }
+}
---
+++ src/Registry.java
@@ -0,0 +1,15 @@
+package src;
+import java.util.ArrayList;
+
+/** 
+ * Represents the collection of litterature
+ * <p>
+ * With this registry you can:
+ * <ul>
+ * <li> Add book(s) to the registry.
+ * <li> List the contents of the Registry.
+ * <li> Search the registry for title. 
+ * <li>
+ *  
+ */
+ public class Registry {}
