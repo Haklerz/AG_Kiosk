9a84b26e8c9e78fc314022c42534291d5a49dfb8
MrRazuuus <trymelym13@gmail.com>
2019-03-06 10:09:57 +0100

    added test classes

---
+++ Register/ApplicationUITest.java
@@ -0,0 +1,42 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class ApplicationUITest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class ApplicationUITest
+{
+    /**
+     * Default constructor for test class ApplicationUITest
+     */
+    public ApplicationUITest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
