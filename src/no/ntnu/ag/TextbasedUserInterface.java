0bc989140dfeb825f865a73f8a26fe95d6a8624c
Haklerz <haakon.lervik@gmail.com>
2019-03-25 21:32:46 +0100

    List method

--- src/no/ntnu/ag/TextbasedUserInterface.java
+++ src/no/ntnu/ag/TextbasedUserInterface.java
@@ -1,23 +1,26 @@
 package no.ntnu.ag;
 
 import java.util.Scanner;
+import java.util.Iterator;
+import no.ntnu.ag.literature.*;
 
 /**
  * TextbasedUserInterface
+ * <ul>
+ * <li>Det skal være mulig å registrere lesestoff av alle typer definert
+ * ovenfor.
+ * <li>Det skal være mulig å skrive ut innholdet i hele registeret.
+ * <li>Det skal være mulig å søke i registeret på tittel og utgiver for å finne
+ * ett konkret lesestoff.
+ * <li>Det skal være mulig å søke i registeret etter alt lesestoff fra en
+ * bestemt utgiver.
+ * <li>Det skal være mulig å gjøre om en enkeltstående bok om til en serie.
+ * <li>I tillegg kan dere utvide applikasjonen med funksjonalitet dere mener
+ * hører hjemme i en slik applikasjon som for eksempel mer avansert søk, bygge
+ * inn salgs-støtte (kassa-apparat (POS)) osv.
+ * </ul>
  * 
- * Det skal være mulig å registrere lesestoff av alle typer definert ovenfor.
- * Det skal være mulig å skrive ut innholdet i hele registeret.
- * Det skal være mulig å søke i registeret på tittel og utgiver for å finne ett konkret lesestoff.
- * Det skal være mulig å søke i registeret etter alt lesestoff fra en bestemt utgiver.
- * Det skal være mulig å gjøre om en enkeltstående bok om til en serie.
- * I tillegg kan dere utvide applikasjonen med funksjonalitet dere mener hører hjemme i en slik
- * applikasjon som for eksempel mer avansert søk, bygge inn salgs-støtte (kassa-apparat (POS)) osv.
- * 
- * new
- * list
- * find
- * add
- * remove
+ * new list find add remove
  * 
  * @version 2019.3.25
  * @author Håkon "Haklerz" Lervik
@@ -41,6 +44,10 @@ public class TextbasedUserInterface {
             Instruction instruction = Instruction.parseInstruction(userInput);
 
             switch (instruction.getCommand()) {
+            case "list":
+                list();
+                break;
+
             case "quit":
                 quit();
                 break;
@@ -48,6 +55,24 @@ public class TextbasedUserInterface {
             default:
                 printUnknownCommand();
                 break;
+            }
+        }
+    }
+
+    private void list() {
+        Iterator<Literature> literatureIterator = registry.getLiteratureIterator();
+        while (literatureIterator.hasNext()) {
+            Literature literature = literatureIterator.next();
+            if (literature instanceof Book) {
+
+            } else if (literature instanceof Journal) {
+
+            } else if (literature instanceof Newspaper) {
+            
+            } else if (literature instanceof Magazine) {
+
+            } else if (literature instanceof BookSeries) {
+                
             }
         }
     }
@@ -61,6 +86,7 @@ public class TextbasedUserInterface {
     }
 
     private void quit() {
+        System.out.println("Thank you for using kiosk v" + 0.6 + ".");
         this.running = false;
         this.input.close();
     }
