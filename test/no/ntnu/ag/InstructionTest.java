623fd55fa012d88b916464f6a75ee241d95f0f48
Haklerz <haakon.lervik@gmail.com>
2019-04-24 09:13:23 +0200

    yeet

---
+++ test/no/ntnu/ag/InstructionTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * InstructionTest
+ */
+public class InstructionTest {
+    @Test
+    public void testGetArgument() {
+        assertEquals(true, true);
+    }
+    
+}
